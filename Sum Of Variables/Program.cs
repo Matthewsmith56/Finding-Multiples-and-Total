e0d36146c0f1bf73e00304f07665783764f32ee1
Matthew <Matthew@DESKTOP-FBBVVQH>
2018-09-26 09:27:03 -0400

    Final Program

--- Sum Of Variables/Program.cs
+++ Sum Of Variables/Program.cs
@@ -8,8 +8,22 @@ namespace Sum_Of_Variables
 {
     class Program
     {
+        //If we list all the natural numbers below 10 that are multiples
+        //of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+        //Find the sum of all the multiples of 3 or 5 below 1000.
+
         static void Main(string[] args)
         {
+            int numTotal = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    numTotal += i;
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("The total is: " + numTotal);
         }
     }
 }
